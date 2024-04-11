46d7d422fffe0bff956bb5237ebdd0162648bed8
Nikolay Osadchyi <148019339+Kelmarji@users.noreply.github.com>
2024-04-11 14:31:17 +0300

    updHeaderLOGEDORNO

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,14 +1,29 @@
 import React from 'react';
 
+import Ava from '../CreateProfilePage/Rectangle 1.png';
+
 import h from './Header.module.scss';
 
 const Header = () => {
+  const isLoged = true;
   return (
     <div className={h.HeaderBase}>
       <span>Realworld Blog</span>
       <div className={h.HeaderBtnGroup}>
-        <button className={h.HeaderBtn}>sign in</button>
-        <button className={[h.HeaderBtn, h.signUp].join(' ')}>sing up</button>
+        {isLoged ? (
+          <div className={isLoged ? [h.HeaderBtnGroup, h.LogedBtn].join(' ') : h.HeaderBtnGroup}>
+            <button className={[h.HeaderBtn, h.signUp, h.CreateBtn].join(' ')}>Create Article</button>
+            <span>Jonh Doe</span>
+            <img src={Ava} />
+
+            <button className={[h.HeaderBtn, h.LogoutBtn].join(' ')}>Log out</button>
+          </div>
+        ) : (
+          <div className={h.HeaderBtnGroup}>
+            <button className={h.HeaderBtn}>Sign in</button>
+            <button className={[h.HeaderBtn, h.signUp].join(' ')}>Sing up</button>
+          </div>
+        )}
       </div>
     </div>
   );
