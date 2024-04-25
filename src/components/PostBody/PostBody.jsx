9e80b977cf0bcad38e90af72e2a023bcd66690fa
Nikolay Osadchyi <148019339+Kelmarji@users.noreply.github.com>
2024-04-25 09:21:30 +0300

    lel

--- src/components/PostBody/PostBody.jsx
+++ src/components/PostBody/PostBody.jsx
@@ -36,14 +36,21 @@ const PostBody = ({ slug }) => {
   const username = author ? author.username : '';
   const image = author ? author.image : '';
 
-  const renderTags = (arr) => arr.map((item, index) => item !== '' ? <li key={index} className={s.tag}>{item}</li> : null);
+  const renderTags = (arr) =>
+    arr.map((item, index) =>
+      item !== '' ? (
+        <li key={index} className={s.tag}>
+          {item}
+        </li>
+      ) : null
+    );
   const [id] = useState(Math.floor(Math.random() * 10));
   const changeLike = (value) => {
     setLiked(!value);
   };
-  const titler = (titl = 'untitled') => titl.length > 35 ? `${titl.slice(0, 35)}...` : titl;
-  const descr = (desc = 'no descript') => desc.length > 150 ? `${desc.slice(0, 150)}` : desc;
-  const validate = (txt,func) => txt ? func(txt) : console.log('gg',slug, txt, 'gnida ebanaya');
+  const titler = (titl = 'untitled') => (titl.length > 35 ? `${titl.slice(0, 35)}...` : titl);
+  const descr = (desc = 'no descript') => (desc.length > 150 ? `${desc.slice(0, 150)}` : desc);
+  const validate = (txt, func) => (txt ? func(txt) : 'untiled/undescrtipted');
 
   if (!loaded) {
     return <Spin />;
@@ -60,38 +67,37 @@ const PostBody = ({ slug }) => {
       <div className={s.HeaderItem}>
         <div className={s.HeaderTitle}>
           <div className={s.HeaderLeft}>
-            <Link style={{textDecoration: 'none'}} key={slug} to={`/articles/${slug}`}>
+            <Link style={{ textDecoration: 'none' }} key={slug} to={`/articles/${slug}`}>
               <h2>{validate(title, titler)}</h2>
             </Link>
-            <div 
-              style={{paddingLeft: '10px', width:'auto', cursor:'pointer'}}
+            <div
+              style={{ paddingLeft: '10px', width: 'auto', cursor: 'pointer' }}
               onClick={() => {
-                BlogApi.unlikeLikePost(liked, token, slug)
-                  .then((articl) => setLiked(articl.favorited));
+                BlogApi.unlikeLikePost(liked, token, slug).then((articl) => setLiked(articl.favorited));
                 changeLike(!liked);
-              }}>
-              <label onClick={() => changeLike(!liked)} className={favorited ? s.LikeCounterCheked : s.LikeCounterUncheked} htmlFor={id}>
-                <span>
-                  {favoritesCount > 0 ? favoritesCount : 0}</span>
+              }}
+            >
+              <label
+                onClick={() => changeLike(!liked)}
+                className={favorited ? s.LikeCounterCheked : s.LikeCounterUncheked}
+                htmlFor={id}
+              >
+                <span>{favoritesCount > 0 ? favoritesCount : 0}</span>
               </label>
             </div>
           </div>
-          <ul className={s.tags}>
-            {renderTags(tagList.slice(0, 6))}
-          </ul>
+          <ul className={s.tags}>{renderTags(tagList.slice(0, 6))}</ul>
         </div>
         <div className={s.HeaderRight}>
           <div>
             <h3>{username}</h3>
             <span>{format(new Date(createdAt), 'MMMM dd, yyyy')}</span>
           </div>
-          <img style={{borderRadius: '50%'}} src={image} alt={username} />
+          <img style={{ borderRadius: '50%' }} src={image} alt={username} />
         </div>
       </div>
       <div className={s.DescPost}>
-        <span className={s.DescPost}>
-          {validate(description, descr)}
-        </span>
+        <span className={s.DescPost}>{validate(description, descr)}</span>
       </div>
     </li>
   );
